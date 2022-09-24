18d093e6d3be94ab3134d74b8871587801567d9b
Wasberth <wdavidgp@hotmail.com>
2022-09-23 21:26:36 -0500

    Follow up on meme commands

--- src/commands/tools/meme.js
+++ src/commands/tools/meme.js
@@ -1,42 +1,178 @@
 const {
-    SlashCommandBuilder,
-    EmbedBuilder,
-    ComponentType,
-    ActionRowBuilder,
-    SelectMenuBuilder
-}   = require('discord.js');
+	SlashCommandBuilder,
+	EmbedBuilder,
+	Client,
+	CommandInteraction,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	ComponentType,
+} = require("discord.js");
+
+const MemeTemplate = require("./../../misc/classes/memeTemplate.js");
+
+const pageDensity = 25;
+
+const {
+	getMemeTemplates,
+	updateMemeTemplates,
+} = require("./../../misc/memeGenerator/memeTemplateHandler");
+
+const createEmbed = (client) => {
+	return new EmbedBuilder()
+		.setColor(0x0099ff)
+		.setAuthor({
+			name: "Calidad Cyberpunk",
+			iconURL:
+				"https://cdn.discordapp.com/emojis/771974836322959360.webp",
+		})
+		.setThumbnail(client.user.avatarURL())
+		.setFooter({
+			text: "Bot hecho con calidad por Queso y Pan de piña",
+			iconURL:
+				"https://cdn.discordapp.com/emojis/1001666667828486234.webp",
+		});
+};
+
+/**
+ * @param {Number} page
+ */
+const embedPage = async (client, page) => {
+	let templates = await getMemeTemplates();
+	let embed = createEmbed(client).setTitle("Lista de memes:");
+
+	for (let i = 0; i < pageDensity; i++) {
+		let template = templates[(page - 1) * pageDensity + i];
+
+		if (!template) {
+			break;
+		}
+
+		embed.addFields({
+			name: template.id,
+			value: `${template.name}\n${template.blank}`,
+		});
+	}
+
+	return embed;
+};
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('meme')
-        .setDescription('Crea un meme personalizado'),
-    async execute(interaction, client) {
+	data: new SlashCommandBuilder()
+		.setName("meme")
+		.setDescription("Crea un meme personalizado")
+		.addSubcommand((subcommand) => {
+			return subcommand
+				.setName("list")
+				.setDescription("Te muestra la lista de memes posibles")
+				.addIntegerOption((option) => {
+					return option
+						.setName("pagina")
+						.setDescription("La página de memes que se puede poner")
+						.setRequired(true);
+				});
+		})
+		.addSubcommand((subcommand) => {
+			return subcommand
+				.setName("create")
+				.setDescription("Te muestra la lista de memes posibles");
+		})
+		.addSubcommand((subcommand) => {
+			return subcommand
+				.setName("update-list")
+				.setDescription("Te muestra la lista de memes posibles");
+		}),
 
-        const selector = new ActionRowBuilder()
-            .addComponents(
-                new SelectMenuBuilder()
-                    .setCustomId('memeSelector')
-                    .setPlaceholder('Seleccione un meme')
-                    .addOptions(client.memeSelectorOptions.slice(0, 25))
-            );
-        
-        const embed = new EmbedBuilder()
-            .setColor(0x0099FF)
-            .setTitle('Interfaz con calidad')
-            .setAuthor({ name: 'Calidad Cyberpunk', iconURL: 'https://cdn.discordapp.com/emojis/771974836322959360.webp' })
-            .setThumbnail(client.user.avatarURL())
-            .addComponents(
-                selector
-            )
-            .setFooter({ text: 'Bot hecho con calidad por Queso y Pan de piña', iconURL: 'https://cdn.discordapp.com/emojis/1001666667828486234.webp' });
+	/**
+	 * @param {CommandInteraction} interaction
+	 * @param {Client} client
+	 */
+	async execute(interaction, client) {
+		let subcommand = interaction.options._subcommand;
+		let templates = await getMemeTemplates();
+		let components = [];
 
-        const message = await interaction.deferReply({
-            fetchReply: true
-        });
+		const embed = createEmbed(client);
 
-        await interaction.editReply({
-            embeds: [embed],
-            //components: [selector]
-        });
-    }
-}
+		switch (subcommand) {
+			case "list":
+				const buttons = new ActionRowBuilder().addComponents(
+					new ButtonBuilder()
+						.setCustomId("previous")
+						.setEmoji("◀️")
+						.setStyle(ButtonStyle.Secondary),
+					new ButtonBuilder()
+						.setCustomId("next")
+						.setEmoji("▶️")
+						.setStyle(ButtonStyle.Secondary)
+				);
+
+				components.push(buttons);
+
+				let pages = Math.ceil(templates.length / pageDensity);
+				let selPage = interaction.options._hoistedOptions[0].value;
+
+				if (pages < selPage || selPage < 1) {
+					await interaction.deferReply({
+						fetchReply: true,
+						ephemeral: true,
+					});
+
+					await interaction.editReply({
+						content: `Hay un máximo de ${pages} páginas`,
+					});
+
+					return;
+				}
+
+				const listMessage = await interaction.deferReply({
+					fetchReply: true,
+				});
+
+				await interaction.editReply({
+					embeds: [await embedPage(client, selPage)],
+					components: components,
+				});
+
+				const listCollector =
+					listMessage.createMessageComponentCollector({
+						componentType: ComponentType.Button,
+					});
+
+				listCollector.on("collect", async (i) => {
+					selPage += i.customId == "next" ? 1 : -1;
+					listMessage.edit({
+						embeds: [await embedPage(client, selPage)],
+						components: components,
+					});
+					return i;
+				});
+
+				return;
+			case "create":
+				break;
+			case "update-list":
+				await updateMemeTemplates();
+
+				const messageUpdate = await interaction.deferReply({
+					fetchReply: true,
+					ephemeral: true,
+				});
+
+				await interaction.editReply({
+					content: ":white_check_mark:",
+				});
+
+				return;
+		}
+
+		const message = await interaction.deferReply({
+			fetchReply: true,
+		});
+
+		await interaction.editReply({
+			embeds: [embed],
+			components: components,
+		});
+	},
+};
--- src/misc/memeGenerator/memeTemplateHandler.js
+++ src/misc/memeGenerator/memeTemplateHandler.js
@@ -1,6 +1,33 @@
 const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+	import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-const TEMPLATES_URL = 'https://api.memegen.link/templates/';
-const SETTINGS = {method: 'Get'};
+const MemeTemplate = require("./../classes/memeTemplate.js");
 
+const TEMPLATES_URL = "https://api.memegen.link/templates/";
+const SETTINGS = { method: "Get" };
+
+let memeTemplates = [];
+
+/**
+ * 
+ * @returns {Promise<Array<MemeTemplate>>}
+ */
+const updateMemeTemplates = async () => {
+	return memeTemplates = await fetch(TEMPLATES_URL, SETTINGS)
+		.then((res) => res.json());
+};
+
+/**
+ * 
+ * @returns {Promise<Array<MemeTemplate>>}
+ */
+ const getMemeTemplates = async () => {
+	if (memeTemplates = []) {
+		return await updateMemeTemplates();
+	}
+
+	return memeTemplates;
+};
+
+module.exports.getMemeTemplates = getMemeTemplates;
+module.exports.updateMemeTemplates = updateMemeTemplates;
